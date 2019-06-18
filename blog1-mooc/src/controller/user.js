cf64f35c49ce63380a7981f452bf5481748aa6cf
longjincen <2418204619@qq.com>
2019-06-18 11:19:06 +0800

    feat: mysql注入攻击 注入攻击一般都是通过提交表单等，不提交正常数据，通过提交mysql语句，从在后端使用提交的信息时发生危险。 可以通过mysql.escape来转仪每一个需要拼接到mysql语句中的变量

--- blog1-mooc/src/controller/user.js
+++ blog1-mooc/src/controller/user.js
@@ -1,7 +1,9 @@
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
 
 const login = (username, password) => {
-    const sql = `select username, realname from users where username='${username}' and password='${password}';`
+    username = escape(username)
+    password = escape(password)
+    const sql = `select username, realname from users where username=${username} and password=${password};`
     return exec(sql).then(rows => {
         if(rows[0] && rows[0].username) {
             
