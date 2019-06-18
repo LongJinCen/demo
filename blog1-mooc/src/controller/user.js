f6789cd7cd4400e6ceca34f50bdc364b09cfd42a
longjincen <2418204619@qq.com>
2019-06-18 12:06:00 +0800

    feat: 密码加密 为了防止数据库数据泄漏导致的用户信息泄露，需要对用户名的密码进行加密。 使用nodejs提供的crpyto模块，在用户注册的时候和用户登陆的时候对密码进行加密。

--- blog1-mooc/src/controller/user.js
+++ blog1-mooc/src/controller/user.js
@@ -1,9 +1,10 @@
 const { exec, escape } = require('../db/mysql')
+const { getPassword } = require('../util/crypto')
 
 const login = (username, password) => {
     username = escape(username)
-    password = escape(password)
-    const sql = `select username, realname from users where username=${username} and password=${password};`
+    password = getPassword(escape(password))
+    const sql = `select username, realname from users where username=${username} and password='${password}';`
     return exec(sql).then(rows => {
         if(rows[0] && rows[0].username) {
             
